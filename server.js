6438a18321cf73618455adac13394366b9044154
Glitch (plucky-krill) <none>
2018-09-06 10:28:42 +0000

    🐏🤒 Checkpoint ./server.js:1694147/2058 ./views/index.html:1694147/716

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-// server.js
-// where your node app starts
 
-// init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -15,8 +14,25 @@ app.use(express.static('public'));
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
+        
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+/* 
+  This is an endpoint that Intercom will POST HTTP request when the card needs to be initialized.
+  This can happen when your teammate inserts the app into a conversation composer, Messenger home settings or User Message.
+  Params sent from Intercom contains for example `card_creation` parameter that was formed by your `configure` response.
+*/
+app.post("/initialize", (request, response) => {  
+  const body = request.body;  
+  response.send({
+    canvas: {
+      content: {
+        components: [
+          { type: "button", label: "Click ME", style: "primary", id: "url_button", action: {type: "submit"} },
+        ], 
+      },
+    },
+  });
 });
