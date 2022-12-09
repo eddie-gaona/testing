86cb00dee5913e5dc02cb2096199318c8b0a515b
Glitch (plucky-krill) <none>
2022-12-09 19:23:54 +0000

    🏃💝 Checkpoint ./views/index.html:51057597/16 ./server.js:51057597/30288

--- server.js
+++ server.js
@@ -35,3 +35,105 @@ app.post("/initialize", (request, response) => {
     },
   });
 });
+
+app.post("/submit", (request, response) => {
+  const body = request.body;
+  response.send({
+    canvas: {
+      content: {
+        components: [
+                      {
+                        "type": "text",
+                        "text": "*Create a ticket*",
+                        "style": "header"
+                      },
+                      {
+                        "type": "input",
+                        "id": "title",
+                        "label": "Title",
+                        "placeholder": "Enter a title for your issue..."
+                      },
+                      {
+                        "type": "input",
+                        "id": "Email",
+                        "label": "Customer Email",
+                        "placeholder": "Enter an email for your issue..."
+                      },
+                      {
+                        "type": "textarea",
+                        "id": "description",
+                        "label": "Description",
+                        "placeholder": "Enter a description of the issue..."
+                      },
+                      {
+                        "type": "dropdown",
+                        "id": "label",
+                        "label": "Request Type",
+                        "options": [
+                          {
+                            "type": "option",
+                            "id": "bug",
+                            "text": "Bug"
+                          },
+                          {
+                            "type": "option",
+                            "id": "bug-critical",
+                            "text": "Bug Critical Issue"
+                          },
+                          {
+                            "type": "option",
+                            "id": "feedback",
+                            "text": "Feedback"
+                          },
+                          {
+                            "type": "option",
+                            "id": "troubleshooting",
+                            "text": "Troubleshooting"
+                          },
+                          {
+                            "type": "option",
+                            "id": "conceptual",
+                            "text": "Conceptual"
+                          }
+                        ]
+                      },
+                      {
+                        "type": "single-select",
+                        "id": "priority",
+                        "label": "Priority",
+                        "options": [
+                          {
+                            "type": "option",
+                            "id": "low",
+                            "text": "Low"
+                          },
+                          {
+                            "type": "option",
+                            "id": "medium",
+                            "text": "Medium"
+                          },
+                          {
+                            "type": "option",
+                            "id": "high",
+                            "text": "High"
+                          }
+                        ]
+                      },
+                      {
+                        "type": "spacer",
+                        "size": "s"
+                      },
+                      {
+                        "type": "button",
+                        "id": "submit-issue-form",
+                        "label": "Create issue",
+                        "style": "primary",
+                        "action": {
+                          "type": "submit"
+                        }
+                      }
+                  ],
+      },
+    },
+  });
+});
