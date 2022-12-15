617f42745495c3124226d4159805426e3dce3b87
Glitch (plucky-krill) <none>
2022-12-15 22:05:44 +0000

    🚀⚡️ Checkpoint ./server.js:52168781/13649

--- server.js
+++ server.js
@@ -10,10 +10,174 @@ app.use(express.static(__dirname));
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+var zendeskID = '';
+var zenID = [];
+//Intercom Get Conversation details
+const getConvo = (getID) => {
+
+//console.log(getID); //Logs the ID of the conversation to the console
+
+var options = {
+  hostname: 'api.intercom.io',
+  path: '/conversations/' + getID,
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': '*/*',
+    'Authorization': "Bearer dG9rOmNiOTNiODIzX2YzMjFfNDIyOF85MTFmX2IyMjg0NTg4YWYxZToxOjA="
+  }
+};
+  let data = '';
+
+  const request = https.request(options, (response) => {
+    // Set the encoding, so we don't get log to the console a bunch of gibberish binary data
+    response.setEncoding('utf8');
+
+    // As data starts streaming in, add each chunk to "data"
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    response.on('end', () => {
+      //console.log(data);
+      createTicket(data, getID);
+    });
+  });
+  // Log errors if any occur
+  request.on('error', (error) => {
+      console.error(error);
+  });
+  // End the request
+  request.end();
+};
+
+
+
+
+//Intercom Get conversation details ends
+const createTicket = (dataIntercom, getID) => {
+  var dataInt = dataIntercom;
+  var parsedJSON = JSON.parse(dataInt);
+
+  var dataInt2 = JSON.parse(JSON.stringify(parsedJSON.conversation_parts.conversation_parts));
+  let result;
+  for (var i = 0; i<dataInt2.length; ++i) {
+    if(dataInt2[i]["body"] == null) {
+      
+    } else {
+        //result = dataInt2[0]["body"].concat(" ", dataInt2[i]["body"]);
+    }
+  }
+  let text = [parsedJSON.source.body];
+  for(var i = 0; i<dataInt2.length; ++i) {
+    text.push(dataInt2[i]["body"])
+  }
+  let msg = text.join(' ');
+
+
+var inputBody = {
+  "ticket": {
+    "requester": "eddie.gaona@amplitude.com",//parsedJSON.source.author.email,
+    "comment": {
+      "html_body": msg
+    }
+  }
+};
+var options = {
+  hostname: 'amplitude.zendesk.com',
+  path: '/api/v2/tickets',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': "Basic ZWRkaWUuZ2FvbmFAYW1wbGl0dWRlLmNvbS90b2tlbjo3QVdmdDBqZHRFZ1ByU1hIajVpTFNwS252NmwyYzVibXgycTVyY1FQ"
+  }
+};
+
+//console.log(inputBody)
+  let data = '';
+
+  const request = https.request(options, (response) => {
+    // Set the encoding, so we don't get log to the console a bunch of gibberish binary data
+    response.setEncoding('utf8');
+
+    // As data starts streaming in, add each chunk to "data"
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    response.on('end', () => {
+      //console.log(data);
+      internalComment(data, getID)
+    });
+  });
+
+  // Log errors if any occur
+  request.on('error', (error) => {
+      console.error(error);
+  });
+  request.write(JSON.stringify(inputBody));
+  // End the request
+  request.end();
+}
+
+const internalComment = (dataZen, getID) => {
+  var dataInt = dataZen;
+  var parsedJSON = JSON.parse(dataInt);
+  var zenID = parsedJSON["ticket"]["id"];
+  
+  var options = {
+  hostname: 'api.intercom.io',
+  path: '/conversations/' + getID + '/reply',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': "Bearer dG9rOmNiOTNiODIzX2YzMjFfNDIyOF85MTFmX2IyMjg0NTg4YWYxZToxOjA="
+  }
+};
+  
+  var inputBody = {
+  "message_type": "note",
+  "type": "admin",
+  "admin_id": "5112554",
+  "body": "https://amplitude.zendesk.com/agent/tickets/" + zenID
+};
+  
+  let data = '';
+
+  const request = https.request(options, (response) => {
+    // Set the encoding, so we don't get log to the console a bunch of gibberish binary data
+    response.setEncoding('utf8');
+
+    // As data starts streaming in, add each chunk to "data"
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    response.on('end', () => {
+      console.log(data);
+    });
+  });
+  // Log errors if any occur
+  request.on('error', (error) => {
+      console.error(error);
+  });
+  // End the request
+  request.write(JSON.stringify(inputBody));
+  request.end();
+  
+}
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/', function (request, response,html) {
+  console.log(response.sendFile(__dirname+'/views/index.html'));
 });
 
 const listener = app.listen(process.env.PORT, () => {
@@ -51,96 +215,17 @@ app.post("/initialize", (request, response) => {
   });
 });
 
-const getConvo = (getID) => {
-
-//console.log(getID); //Logs the ID of the conversation to the console
-var options = {
-  hostname: 'api.intercom.io',
-  path: '/conversations/' + getID,
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': '*/*',
-    'Authorization': "Bearer dG9rOjViMWQ3YjM3X2U1NjFfNGFjZF9hOGIyX2U0MDI2ZTQ1YThkMDoxOjA="
-  }
-};
-  let data = '';
-
-  const request = https.request(options, (response) => {
-    // Set the encoding, so we don't get log to the console a bunch of gibberish binary data
-    response.setEncoding('utf8');
-
-    // As data starts streaming in, add each chunk to "data"
-    response.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    // The whole response has been received. Print out the result.
-    response.on('end', () => {
-      //console.log(data);
-      return 5
-    });
-  });
-  // Log errors if any occur
-  request.on('error', (error) => {
-      console.error(error);
-  });
-  // End the request
-  request.end();
-};
-
-const myFunct = (getID) => {
-  var options = {
-  hostname: 'api.intercom.io',
-  path: '/conversations/' + getID,
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': '*/*',
-    'Authorization': "Bearer dG9rOjViMWQ3YjM3X2U1NjFfNGFjZF9hOGIyX2U0MDI2ZTQ1YThkMDoxOjA="
-  }
-};
-  let data = '';
-
-  const request = https.request(options, (response) => {
-    // Set the encoding, so we don't get log to the console a bunch of gibberish binary data
-    response.setEncoding('utf8');
-
-    // As data starts streaming in, add each chunk to "data"
-    response.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    // The whole response has been received. Print out the result.
-    response.on('end', () => {
-      //console.log(data);
-      return 5
-    });
-    console.log(data);
-  });
-  // Log errors if any occur
-  request.on('error', (error) => {
-      console.error(error);
-  });
-  // End the request
-  request.end();
-}
-
-app.post("/submit", (request, response, next) => {
-  next();
-  const body = request.body;
-  next();
-  var x = getConvo(body['conversation']['id']);
-  next();
-  var y = myFunct(body['conversation']['id']);
-  next();
-  console.log(x);
+app.post("/submit", (request, response, x) => {
+  const body = response.body;
+  var getID = request.body;
+  var id = getID['conversation']['id'];
+  getConvo(id);
   response.send({
     canvas: {
       content: {
         components: [
-          { type: "text", text: "HI WORLD",
-           style: "header", align: "center" },
+          { type: "text", text: "Ticket Submitted",
+           style: "header", align: "center" }
         ],
               },
           },
